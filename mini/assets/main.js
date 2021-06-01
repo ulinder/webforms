b98ba063b9c1a437789fa4a8056537dc967885a6
Johnny Ulinder <julinder@gmail.com>
2021-06-01 12:36:05 +0200

    native js only

--- mini/assets/main.js
+++ mini/assets/main.js
@@ -1,30 +1,23 @@
-// window.addEventListener('load', function () { // Detta körs när sidan indikerar att den laddat klart
-
-//   $.getJSON( 'out.json', (obj)=>{
-//     console.log(obj);
-//   } )
-  
-// })
-
-$(document).ready(()=>{
+window.addEventListener('load', function () {
   
   var current_id = "";
+  var target_div = document.getElementById('formbody');
 
   for (var i = 0; i < DATA.length; i++) {
 
     if(DATA[i].id != null && DATA[i].id != current_id){ // new ID-block
-      $("#formbody").append(`<hr>`);
+      // target_div.innerHTML = target_div.innerHTML + `<hr>`);
+      target_div.innerHTML = target_div.innerHTML + '<hr>';
       current_id = DATA[i].id
     }
 
-
-    // if(DATA[i].id){ $("#formbody").append(`<h3>${DATA[i].id}</h3>`); }    
-    if(DATA[i].subid){ $("#formbody").append(`<h3>${current_id} ${DATA[i].subid}</h3>`); }
+    if(DATA[i].subid){ target_div.innerHTML = target_div.innerHTML + `<h3>${current_id} ${DATA[i].subid}</h3>` }
     
-    if(DATA[i].title){ $("#formbody").append(`<h2>${DATA[i].title}</h2>`); }
-    if(DATA[i].text){ $("#formbody").append(`<p>${DATA[i].text}</p>`); }
+    if(DATA[i].title){ target_div.innerHTML = target_div.innerHTML + `<h2>${DATA[i].title}</h2>` }
+      
+    if(DATA[i].text){ target_div.innerHTML = target_div.innerHTML + `<p>${DATA[i].text}</p>` }
 
-    if(DATA[i].answer){ $("#formbody").append(`<input type="radio" name="radio${i}"> NEJ <input type="radio" name="radio${i}"> JA`); }
+    if(DATA[i].answer){ target_div.innerHTML = target_div.innerHTML + `<input type="radio" name="radio${i}"> NEJ <input type="radio" name="radio${i}"> JA` }
 
   }
 
