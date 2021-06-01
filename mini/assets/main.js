529665877fcaa58a8316b3bd150c2bdaf3f43f34
Johnny Ulinder <julinder@gmail.com>
2021-06-01 11:51:08 +0200

    Update main.js

--- mini/assets/main.js
+++ mini/assets/main.js
@@ -7,17 +7,21 @@
 // })
 
 $(document).ready(()=>{
+  
+  var current_id = "";
 
-  console.log('Write json content')
   for (var i = 0; i < DATA.length; i++) {
 
+    if(DATA[i].id != null && DATA[i].id != current_id){ // new ID-block
+      $("#formbody").append(`<hr>`);
+      current_id = DATA[i].id
+    }
+
+
+    // if(DATA[i].id){ $("#formbody").append(`<h3>${DATA[i].id}</h3>`); }    
+    if(DATA[i].subid){ $("#formbody").append(`<h3>${current_id} ${DATA[i].subid}</h3>`); }
+    
     if(DATA[i].title){ $("#formbody").append(`<h2>${DATA[i].title}</h2>`); }
-    
-    if(DATA[i].id){ $("#formbody").append(`<h2>${DATA[i].id}</h2>`); }
-    
-    if(DATA[i].subid){ $("#formbody").append(`<h3>${DATA[i].subid}</h3>`); }
-    
-    
     if(DATA[i].text){ $("#formbody").append(`<p>${DATA[i].text}</p>`); }
 
     if(DATA[i].answer){ $("#formbody").append(`<input type="radio" name="radio${i}"> NEJ <input type="radio" name="radio${i}"> JA`); }
