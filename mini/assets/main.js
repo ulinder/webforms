9771c1ab214bdbb6ff10b0587e5c6f5ff11fe9b5
Johnny Ulinder <julinder@gmail.com>
2021-06-03 10:47:23 +0200

    faster script + move raw files

--- mini/assets/main.js
+++ mini/assets/main.js
@@ -3,21 +3,23 @@ window.addEventListener('load', function () {
   var current_id = "";
   var target_div = document.getElementById('formbody');
 
+  var new_el = (str, valStr) =>{
+    var el = document.createElement(str);
+    if(valStr) el.innerHTML = valStr;
+    return el; 
+  }
+
   for (var i = 0; i < DATA.length; i++) {
 
     if(DATA[i].id != null && DATA[i].id != current_id){ // new ID-block
-      // target_div.innerHTML = target_div.innerHTML + `<hr>`);
-      target_div.innerHTML = target_div.innerHTML + '<hr>';
+      target_div.append( new_el('hr') );
       current_id = DATA[i].id
     }
 
-    if(DATA[i].subid){ target_div.innerHTML = target_div.innerHTML + `<h3>${current_id} ${DATA[i].subid}</h3>` }
-    
-    if(DATA[i].title){ target_div.innerHTML = target_div.innerHTML + `<h2>${DATA[i].title}</h2>` }
-      
-    if(DATA[i].text){ target_div.innerHTML = target_div.innerHTML + `<p>${DATA[i].text}</p>` }
-
-    if(DATA[i].answer){ target_div.innerHTML = target_div.innerHTML + `<input type="radio" name="radio${i}"> NEJ <input type="radio" name="radio${i}"> JA` }
+    if(DATA[i].subid) target_div.append( new_el('h3', `${current_id} ${DATA[i].subid}`) )   
+    if(DATA[i].title) target_div.append( new_el('h2', DATA[i].title) ) 
+    if(DATA[i].text)  target_div.append( new_el('p', DATA[i].text ) )  
+    if(DATA[i].answer){ target_div.append( new_el('p','JA/NEJ') ) } // `<input type="radio" name="radio${i}"> NEJ <input type="radio" name="radio${i}"> JA` }
 
   }
 
