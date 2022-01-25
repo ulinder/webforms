ab3180822b182e337cfeafa1fb8672ae5b80f6f6
Johnny Ulinder <julinder@gmail.com>
2022-01-25 14:57:12 +0100

    add rule

--- mini/assets/rules.js
+++ mini/assets/rules.js
@@ -1,6 +1,7 @@
 // Reglerna ligger lösa i en array där varje objekt innehåller ett test, med en åtgärd. 
 // Det finns X antal regler: display, calculate, 
 RULES = [
-{name: "a1a", type:"display", c:{ targetValue:"1", target: "div_a1b" } },
-{name: "a1b", type:"display", c:{ targetValue:"1", target: "div_a2a" }}
+{name: "a1a", type:"display", c:{ targetValue:"1", target: "div_a1b" }},
+{name: "a1b", type:"display", c:{ targetValue:"1", target: "div_a2a" }},
+{name: "a2a", type:"display", c:{ targetValue:"1", target: "div_a2b" }},
 ]
