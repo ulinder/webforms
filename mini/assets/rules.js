43e2e4d5d00b70b89a6c50dc8f74ae2606bc1c7a
Progmik1337 <73596221+Progmik1337@users.noreply.github.com>
2022-02-03 16:58:13 +0100

    Nya regelformat plus blabla

--- mini/assets/rules.js
+++ mini/assets/rules.js
@@ -3,7 +3,11 @@
 // cond:[] - block av test som ska vara sanna 
 
 RULES = [
-  { trigger:"a1a", cond:[ ["a1a","==",1] ], action:"show", target:"div_a1b" },
-  { trigger:"a1b", cond:[ ["a1a","==",1] ], action:"show", target:"div_a2a" },
-  { trigger:"a2a", cond:[ ["a1a","==",1] ], action:"show", target:"div_a2b" },
+
+  { trigger:"a1a", cond:[ ["a1a","<",2] ], action:"show", target:["div_a1b"]},
+  { trigger:"a1b", cond:[ ["a1b","<",2] ], action:"show", target:["div_a2a"] },
+  { trigger:"a2a", cond:[ ["a2a","<",2] ], action:"show", target:["div_a2b"]},
+  { trigger:"a2b", cond:[ ["a1a","==",0] , ["a2a","==",0]], action:"show", target:["div_b1"]},
+  { trigger:"a2b", cond:[ ["a1b","==",1] , ["a2b","==",1]], action:"show", target:["div_a3"]},
+
 ]
