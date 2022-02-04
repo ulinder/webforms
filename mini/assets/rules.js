e326e67281d3498c2a705dfd81626817180c07cc
Progmik1337 <73596221+Progmik1337@users.noreply.github.com>
2022-02-04 16:31:56 +0100

    Nya MINI och och rules

--- mini/assets/rules.js
+++ mini/assets/rules.js
@@ -5,9 +5,11 @@
 RULES = [
 
   { trigger:"a1a", cond:[ ["a1a","<",2] ], action:"show", target:["div_a1b"]},
-  { trigger:"a1b", cond:[ ["a1b","<",2] ], action:"show", target:["div_a2a"] },
+  { trigger:"a1b", cond:[ ["a1b","<",2] ], action:"show", target:["div_a2a"]},
   { trigger:"a2a", cond:[ ["a2a","<",2] ], action:"show", target:["div_a2b"]},
-  { trigger:"a2b", cond:[ ["a1a","==",0] , ["a2a","==",0]], action:"show", target:["div_b1"]},
-  { trigger:"a2b", cond:[ ["a1b","==",1] , ["a2b","==",1]], action:"show", target:["div_a3"]},
+  { trigger:"a2b", cond:[ ["a1a","==",0] , ["a2a","==",0] ], action:"show", target:["div_b1"]},
+  { trigger:"a2b", cond:[ ["a1a","==",1] , ["a2a","==",1], ["a1b","==",1] , ["a2b","==",1] ], action:"show", target:['div_intro_a3','div_a3a','div_a3a_2w','div_a3a_prev']},
+  { trigger:"a2b", cond:[ ["a1a","==",1] , ["a2a","==",1], ["a1b","==",0] , ["a2b","==",0] ], action:"show", target:['div_intro_a3','div_a3a','div_a3a_prev']},
+
 
 ]
